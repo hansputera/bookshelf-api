c811bff5fc2e3639a826f96858397ff4636babd4
hansputera <hanifdwyputrasembiring@gmail.com>
2022-07-12 06:36:14 +0000

    feat: init hapi server
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
+import Hapi from '@hapi/hapi';
+
 /**
  * Setup/boot up the project
  * @return {Promise<void>}
  **/
 export async function setup() {
-    console.log('Dicoding Bookshelf API');
+    const server = Hapi.server({
+        port: process.env.PORT || 3000,
+        host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
     return;
 }

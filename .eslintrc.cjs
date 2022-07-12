077e7d764173f19f1bcc26c3622a7f570f0aead6
hansputera <hanifdwyputrasembiring@gmail.com>
2022-07-12 06:26:57 +0000

    chore: init eslint, and prettier config
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,12 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+    env: {
+        es2021: true,
+        node: true,
     },
-    "extends": [
-        "google"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ['google'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "rules": {
-    }
-}
+    rules: {},
+};

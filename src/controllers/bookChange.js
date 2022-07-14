05295351f8b0228233adbec5d9db90ec56a89c9a
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-14 14:17:04 +0000

    feat(controllers): added bookChange controller
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/controllers/bookChange.js
@@ -0,0 +1,46 @@
+/** @typedef {import('@hapi/hapi').ServerRoute} ServerRoute */
+/** @typedef {import('@hapi/hapi').Request} Request */
+/** @typedef {import('@hapi/hapi').ResponseToolkit} Response */
+
+import {BookModel} from '../models/Book.js';
+import {bookStores} from '../stores.js';
+
+/** @type {ServerRoute} */
+/**
+ * Book Change Controller.
+ * @param {Request} req - HTTP Request
+ * @param {Response} res - HTTP Response.
+ */
+export const bookChange = async function (req, res) {
+    const book = bookStores.get(req.params.bookId);
+
+    if (!book) {
+        return res
+            .response({
+                status: 'fail',
+                message: 'Gagal memperbarui buku. Id tidak ditemukan',
+            })
+            .code(404);
+    }
+
+    bookStores.set(
+        req.params.bookId,
+        new BookModel(
+            req.payload.name,
+            req.payload.year,
+            req.payload.author,
+            req.payload.summary,
+            req.payload.publisher,
+            req.payload.pageCount,
+            req.payload.readPage,
+            req.payload.reading,
+        ),
+    );
+
+    return res
+        .response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui',
+        })
+        .code(200);
+};

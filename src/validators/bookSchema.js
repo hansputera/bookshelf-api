321d1114d411e6de57092f634d523222a6d5fff8
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-14 13:36:46 +0000

    fix(validators.book): fix custom error message
    Signed-off-by: GitHub <noreply@github.com>

--- src/validators/bookSchema.js
+++ src/validators/bookSchema.js
@@ -3,52 +3,57 @@ import Joi from 'joi';
 export const bookSchema = Joi.object({
     name: Joi.string()
         .required()
-        .error(() => {
-            return {
-                message: 'Gagal menambahkan buku. Mohon isi nama buku',
-            };
-        }),
+        .error(() => new Error('Gagal menambahkan buku. Mohon isi nama buku')),
     year: Joi.number()
         .required()
-        .error(() => ({
-            message: 'Gagal menambahkan buku. Mohon isi tahun rilis buku',
-        })),
+        .error(
+            () =>
+                new Error('Gagal menambahkan buku. Mohon isi tahun rilis buku'),
+        ),
     author: Joi.string()
         .required()
-        .error(() => ({
-            message: 'Gagal menambahkan buku. Mohon isi author buku',
-        })),
+        .error(
+            () => new Error('Gagal menambahkan buku. Mohon isi author buku'),
+        ),
     summary: Joi.string()
         .required()
         .min(5)
-        .error(() => ({
-            message:
-                'Gagal menambahkan buku. Mohon isi summary buku, dan minimal 5 karakter',
-        })),
+        .error(
+            () =>
+                new Error(
+                    'Gagal menambahkan buku. Mohon isi summary buku, dan minimal 5 karakter',
+                ),
+        ),
     publisher: Joi.string()
         .required()
         .min(3)
-        .error(() => ({
-            message: 'Gagal menambahkan buku. Mohon isi publisher buku',
-        })),
+        .error(
+            () => new Error('Gagal menambahkan buku. Mohon isi publisher buku'),
+        ),
     pageCount: Joi.number()
         .required()
         .min(1)
-        .error(() => ({
-            message:
-                'Gagal menambahkan buku. pageCount harus diisi sebagaimana mestinya',
-        })),
+        .error(
+            () =>
+                new Error(
+                    'Gagal menambahkan buku. pageCount harus diisi sebagaimana mestinya',
+                ),
+        ),
     readPage: Joi.number()
         .required()
         .max(Joi.ref('pageCount'))
-        .error(() => ({
-            message:
-                'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-        })),
+        .error(
+            () =>
+                new Error(
+                    'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+                ),
+        ),
     reading: Joi.bool()
         .required()
-        .error(() => ({
-            message:
-                'Gagal menambahkan buku. reading harus diisi dengan boolean',
-        })),
+        .error(
+            () =>
+                new Error(
+                    'Gagal menambahkan buku. reading harus diisi dengan boolean',
+                ),
+        ),
 });
